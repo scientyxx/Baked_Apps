afffae02768ab0ba16818d3b572d34eafd7faafc
scientyxx <srialia110@gmail.com>
2024-06-04 21:21:45 +0700

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        "/": (context) => CoverPage(),
+        // "/": (context) => CoverPage(),
+        "/": (context) => HomePage(),
         "startingpage": (context) => StartingPage(),
         "continuepage": (context) => ContinuePage(),
         "loginpage": (context) => LoginPage(),
